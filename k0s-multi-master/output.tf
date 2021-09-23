c3a35b27ec6bbcae6298c9bdf41ca9b539a2bd99
Aditya Rahman <adityarahman032@gmail.com>
2021-09-23 19:28:09 +0800

    Fix output

--- k0s-multi-master/output.tf
+++ k0s-multi-master/output.tf
@@ -1,33 +1,27 @@
-output "vpc_id" {
-  description = "The ID ofr the VPC"
-  value = module.vpc.vpc_id
-}
-
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value = module.vpc.vpc_cidr_block
-}
-
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value = module.vpc.public_subnets
-}
-
 output "ec2_instance_loadbalancer_public_ip" {
   description = "The public IP address assigned to the instance for loadbalancer"
-  value = module.ec2_instance_loadbalancer.public_ip
+  value = {
+    public_ip = module.ec2_instance_loadbalancer.public_ip
+    private_dns = module.ec2_instance_loadbalancer.private_dns
+  }
 }
 
 output "ec2_instance_master_public_ip" {
   description = "The public IP address assigned to the instance for master"
   value = {
-    for k, v in module.ec2_instance_master : k => v.public_ip
+    for k, v in module.ec2_instance_master : k => {
+      public_ip = v.public_ip
+      private_dns = v.private_dns
+    }
   }
 }
 
 output "ec2_instance_worker_public_ip" {
   description = "The public IP address assigned to the instance for worker"
   value = {
-    for k, v in module.ec2_instance_worker : k => v.public_ip
+    for k, v in module.ec2_instance_worker : k => {
+      public_ip = v.public_ip
+      private_dns = v.private_dns
+    }
   }
 }
